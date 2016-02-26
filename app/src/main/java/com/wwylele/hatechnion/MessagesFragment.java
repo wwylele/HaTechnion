7f18e570e357076ef0834dcd0ab675302348d7d5
wwylele <wwylele@gmail.com>
2016-02-26 13:00:03 +0200

    Add message page

---
+++ app/src/main/java/com/wwylele/hatechnion/MessagesFragment.java
@@ -0,0 +1,24 @@
+package com.wwylele.hatechnion;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MessagesFragment extends Fragment {
+
+
+    public MessagesFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+}
