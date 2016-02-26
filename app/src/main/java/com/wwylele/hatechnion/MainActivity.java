9d3a2c80fcbff3af2919a1130511c9149380755b
wwylele <wwylele@gmail.com>
2016-02-26 13:00:23 +0200

    add pager

--- app/src/main/java/com/wwylele/hatechnion/MainActivity.java
+++ app/src/main/java/com/wwylele/hatechnion/MainActivity.java
@@ -1,12 +1,18 @@
 package com.wwylele.hatechnion;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     public String ticket, username, real;
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,43 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(real + " (" + username + ")");
         setSupportActionBar(toolbar);
 
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+        String[] pageNames = {
+                getString(R.string.title_fragment_grades),
+                getString(R.string.title_fragment_messages)
+        };
+        Class[] pageFragments = {
+                GradesFragment.class,
+                MessagesFragment.class
+        };
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            try {
+                return (Fragment) pageFragments[i].newInstance();
+            } catch (Exception ignored) {
+                return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return pageFragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int i) {
+            return pageNames[i];
+        }
     }
 
 }
